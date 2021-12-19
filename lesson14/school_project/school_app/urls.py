a4d03b3aa0e8d656654a7b43f51347813a84d705
Michal Hucko <michal_hucko@dell.com>
2021-12-19 14:39:24 +0100

    Dev: lesson 14

---
+++ lesson14/school_project/school_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('hello-world', views.hello_world, name="hello-world"),
+    path('subjects', views.list_subjects, name="list-subjects"),
+    path('subjects/<int:pk>', views.subject_detail, name="subject_detail")
+]
